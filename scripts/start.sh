35c575a124a288b5de92d7b6881d7054d13ccef2
MAudelGisaia <mickael.audel@gisaia.com>
2024-12-20 10:11:26 +0100

    fix: correct gram errors

--- scripts/start.sh
+++ scripts/start.sh
@@ -508,11 +508,11 @@ fi
 envsubst 'ARLAS_ENABLE_ADVANCED_FEATURES' < /usr/share/nginx/html/settings.yaml > /usr/share/nginx/html/settings.yaml.tmp
 mv /usr/share/nginx/html/settings.yaml.tmp /usr/share/nginx/html/settings.yaml
 
-# Allow applying an geohash/geotile aggregation on a geoshape field
+# Allow applying a geohash/geotile aggregation on a geoshape field
 if [ -z "${ARLAS_GEOSHAPE_AGG}" ]; then
   ARLAS_GEOSHAPE_AGG=false
   export ARLAS_GEOSHAPE_AGG
-  echo "geohash/geotile aggregation on a geoshape field are not allowed"
+  echo "geohash/geotile aggregation on a geoshape field is not allowed"
 else
   echo ${ARLAS_ENABLE_H3ARLAS_GEOSHAPE_AGG} "is used for 'enable_geoshape_agg' in settings.yaml file"
 fi
