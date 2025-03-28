736699578689ec0d2f12ee6c4a04ed116705f6c2
MAudelGisaia <mickael.audel@gisaia.com>
2025-03-28 16:04:26 +0100

    feature: fix test and provide a way to order tab without tricks

--- src/app/modules/analytics-config/components/edit-resultlist-visualisation/data-group-edition/manage-data-group-dialog.component.spec.ts
+++ src/app/modules/analytics-config/components/edit-resultlist-visualisation/data-group-edition/manage-data-group-dialog.component.spec.ts
@@ -1,4 +1,11 @@
+import {
+  ResultlistFormBuilderService
+} from '@analytics-config/services/resultlist-form-builder/resultlist-form-builder.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { mockProvider } from '@ngneat/spectator';
+import { CollectionService } from '@services/collection-service/collection.service';
+import { TOKEN_LOGGER_CONFIG } from 'ngx-logger';
 
 import { ManageDataGroupDialogComponent } from './manage-data-group-dialog.component';
 
@@ -8,7 +15,13 @@ describe('DataGroupEditionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ManageDataGroupDialogComponent]
+      imports: [ManageDataGroupDialogComponent],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: {}},
+        {provide: TOKEN_LOGGER_CONFIG, useValue: {}},
+        mockProvider(ResultlistFormBuilderService),
+        mockProvider(CollectionService)
+      ]
     })
       .compileComponents();
 
