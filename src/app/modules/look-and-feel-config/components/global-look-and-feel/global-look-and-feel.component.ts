e58c7599ce62fb243b5be10027d3ca7ae04fe9f6
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2025-07-11 12:30:03 +0200

    feat: display all configured collections in collection view

--- src/app/modules/look-and-feel-config/components/global-look-and-feel/global-look-and-feel.component.ts
+++ src/app/modules/look-and-feel-config/components/global-look-and-feel/global-look-and-feel.component.ts
@@ -23,7 +23,6 @@ import {
   LookAndFeelGlobalFormGroup
 } from '@look-and-feel-config/services/look-and-feel-global-form-builder/look-and-feel-global-form-builder.service';
 import { CollectionService } from '@services/collection-service/collection.service';
-import { ConfigExportHelper } from '@services/main-form-manager/config-export-helper';
 import { MainFormService } from '@services/main-form/main-form.service';
 
 @Component({
@@ -35,58 +34,37 @@ export class GlobalLookAndFeelComponent implements OnInit {
 
   public lookAndFeelFg: LookAndFeelGlobalFormGroup;
   public unitsFg: FormArray;
-  public collections: string [];
-  public collectionUnitMap = new Map<string, string>();
+
   public constructor(
-    private mainFormService: MainFormService,
-    private collectionService: CollectionService
+    private readonly mainFormService: MainFormService,
+    private readonly collectionService: CollectionService
   ) {
     this.lookAndFeelFg = this.mainFormService.lookAndFeelConfig.getGlobalFg();
   }
 
   public ngOnInit() {
-    const startingConfig = this.mainFormService.startingConfig.getFg();
-    const mapConfigGlobal = this.mainFormService.mapConfig.getGlobalFg();
-    const mapConfigLayers = this.mainFormService.mapConfig.getLayersFa();
-    const timelineConfigGlobal = this.mainFormService.timelineConfig.getGlobalFg();
-    const searchConfigGlobal = this.mainFormService.searchConfig.getGlobalFg();
-    const analyticsConfigList = this.mainFormService.analyticsConfig.getListFa();
-    const resultLists = this.mainFormService.resultListConfig.getResultListsFa();
-    if (startingConfig.customControls && mapConfigGlobal.customControls && timelineConfigGlobal.customControls
-        && searchConfigGlobal.customControls) {
-      const configuredCollections = ConfigExportHelper.getConfiguredCollections(
-        startingConfig,
-        mapConfigGlobal,
-        mapConfigLayers,
-        searchConfigGlobal,
-        timelineConfigGlobal,
-        analyticsConfigList,
-        resultLists,
-        this.collectionService
-      );
-      /** keeping formarray order */
-      const formArrayCollections = (this.lookAndFeelFg.customControls.units.value as FormArray).controls.map(control =>
-        (control as CollectionUnitFormGroup).customControls.collection.value
-      );
-      const collectionsSet = new Set(configuredCollections);
-      const formSetCollections = new Set(formArrayCollections);
-      const orderedCollections = [];
-      formArrayCollections.forEach(fc => {
-        if (collectionsSet.has(fc)) {
-          orderedCollections.push(fc);
-        }
-      });
-      /** add newly confiured collections at the end */
-      configuredCollections.forEach(c => {
-        if (!formSetCollections.has(c)) {
-          orderedCollections.push(c);
-        }
-      });
-      this.unitsFg = this.lookAndFeelFg.buildUnits(orderedCollections);
-      this.lookAndFeelFg.customControls.units.setValue(this.unitsFg);
-      this.collections = configuredCollections;
+    const configuredCollections = this.mainFormService.getAllCollections(this.collectionService);
 
-    }
+    /** keeping formarray order */
+    const formArrayCollections = (this.lookAndFeelFg.customControls.units.value as FormArray).controls.map(control =>
+      (control as CollectionUnitFormGroup).customControls.collection.value
+    );
+    const collectionsSet = new Set(configuredCollections);
+    const formSetCollections = new Set(formArrayCollections);
+    const orderedCollections = [];
+    formArrayCollections.forEach(fc => {
+      if (collectionsSet.has(fc)) {
+        orderedCollections.push(fc);
+      }
+    });
+    /** add newly configured collections at the end */
+    configuredCollections.forEach(c => {
+      if (!formSetCollections.has(c)) {
+        orderedCollections.push(c);
+      }
+    });
+    this.unitsFg = this.lookAndFeelFg.buildUnits(orderedCollections);
+    this.lookAndFeelFg.customControls.units.setValue(this.unitsFg);
   }
 
 }
