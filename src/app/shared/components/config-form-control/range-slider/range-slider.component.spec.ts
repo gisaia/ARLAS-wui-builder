65cf9058ee5f239ffccfeb08da070389204fd2c2
MAudelGisaia <mickael.audel@gisaia.com>
2026-06-02 14:40:15 +0200

    fix: add good import for test

--- src/app/shared/components/config-form-control/range-slider/range-slider.component.spec.ts
+++ src/app/shared/components/config-form-control/range-slider/range-slider.component.spec.ts
@@ -1,19 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RangeSliderComponent } from './range-slider.component';
-
+import { beforeEach, describe, expect, it } from 'vitest';
+import {TranslateLoader, TranslateModule, TranslateNoOpLoader} from '@ngx-translate/core';
+import {IconFormControl, RangeSliderFormControl} from '@shared-models/config-form';
 describe('RangeSliderComponent', () => {
   let component: RangeSliderComponent;
   let fixture: ComponentFixture<RangeSliderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RangeSliderComponent]
+      imports: [RangeSliderComponent, TranslateModule.forRoot({
+        loader: { provide: TranslateLoader, useClass: TranslateNoOpLoader }
+      })]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(RangeSliderComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('control', new RangeSliderFormControl({}, '',
+        {min:'', max:''}, '0', 1, 2, 1));
     fixture.detectChanges();
   });
 
