6894b49a7f9b6c6cbec5101b8e8f9df572ccad8c
QuCMGisaia <quentin.martin-cocher@gisaia.com>
2026-07-24 16:58:13 +0200

    chore: fix icon-picker test

--- src/app/shared/components/config-icon-picker/config-icon-picker.component.spec.ts
+++ src/app/shared/components/config-icon-picker/config-icon-picker.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl } from '@angular/forms';
+import { TranslateLoader, TranslateModule, TranslateNoOpLoader } from '@ngx-translate/core';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { ConfigIconPickerComponent } from './config-icon-picker.component';
 
 describe('ConfigIconPickerComponent', () => {
@@ -8,12 +10,16 @@ describe('ConfigIconPickerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ConfigIconPickerComponent]
+      imports: [
+        ConfigIconPickerComponent,
+        TranslateModule.forRoot({ loader: { provide: TranslateLoader, useClass: TranslateNoOpLoader } })
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ConfigIconPickerComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('control', new FormControl());
     await fixture.whenStable();
   });
 
